5368b50d8792e3993351dd092f6f4e5a96ab9558
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2024-04-10 19:25:14 -0400

    router stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 import videoSrc from './assets/arcDemo.mp4'; // Path to your video file
 import Navbar from "./components/Navbar.jsx";
 import HomePage from "./components/HomePage.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
 
     return (
-        <div className="bg-gray-50 min-h-screen w-[100vw]" >
-            <Navbar/>
-            <HomePage/>
-        </div>
+        
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<HomePage />} />
+                    <Route path="/projects" element={<HomePage/>} />
+                    <Route path="/team" element={<HomePage/>} />
+                    <Route path="/wiki" element={<HomePage/>} />
+                </Routes>
+            </BrowserRouter>
+
     );
 }
 export default App
