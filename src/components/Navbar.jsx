0da21562b46bdbe9f8d0572ad414e37dc29f7a81
pkurto16 <ptkurto3@gmail.com>
2024-04-09 20:54:14 -0400

    Final Nav Fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import arcLogo from '../assets/arcicon.png';
-
+import './navbar/Navbar.css';
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     useEffect(() => {
@@ -11,36 +12,44 @@ const Navbar = () => {
             const offset = window.scrollY;
             setIsScrolled(offset > 0);
         };
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640);
+        };
 
+        window.addEventListener('resize', handleResize);
         window.addEventListener('scroll', handleScroll);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
     return (
         <>
         <nav className={`bg-white fixed w-full transition-shadow duration-300 ease-in-out ${isScrolled ? 'shadow-lg bg-opacity-90' : 'bg-opacity-100'}`}>
-            <div className="container px-6 py-3 flex justify-between items-center">
+            <div className="px-6 py-3 flex items-center">
                 <Link to="/" className="flex items-center space-x-5">
-                    <img src={arcLogo} alt="Brand Logo" className={`transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'}`}/>
-                    <div className="text-3xl font-bold text-gray-800 hover:text-gray-700">Purdue ARC</div>
+                    <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'}`}/>
+                    <div className="text-3xl font-bold text-gray-800 hover:text-gray-700 text-nowrap">Purdue ARC</div>
                 </Link>
-                <div className="hidden lg:flex flex-grow justify-end items-center space-x-4">
+                <div className="flex-grow"></div>
+                <div className="hidden sm:visible sm:flex items-center space-x-4">
                     <Link to="/" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out">Home</Link>
-                    <Link to="/about" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out">About Us</Link>
+                    <Link to="/about" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out text-nowrap">About Us</Link>
                     <Link to="/projects" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out">Projects</Link>
                     <Link to="/team" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out">Team</Link>
                     <Link to="/wiki" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out">Wiki</Link>
                 </div>
-                <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="lg:hidden text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600">
-                    <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                        <path d="M4 5h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2z"></path>
-                    </svg>
-                </button>
+                {isMobile && (
+                    <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600">
+                        <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                            <path d="M4 5h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2z"></path>
+                        </svg>
+                    </button>
+                )}
             </div>
             {isMobileMenuOpen && (
-                <div className="lg:hidden bg-white shadow-lg absolute w-full">
+                <div className={`sm:hidden bg-white shadow-lg absolute w-full ${isScrolled ? 'shadow-lg bg-opacity-90' : 'bg-opacity-100'}`}>
                     <Link to="/" className="block text-gray-800 hover:text-blue-600 px-6 py-2">Home</Link>
                     <Link to="/about" className="block text-gray-800 hover:text-blue-600 px-6 py-2">About Us</Link>
                     <Link to="/projects" className="block text-gray-800 hover:text-blue-600 px-6 py-2">Projects</Link>
@@ -51,7 +60,7 @@ const Navbar = () => {
         </nav>
             <div className="bg-white">
                 <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                        <img src={arcLogo} alt="Brand Logo" className=" h-10 w-10 opacity-0"/>
+                        <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'} opacity-0`}/>
                 </div>
             </div>
         </>
