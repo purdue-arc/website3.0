fd25a18f71451b042eb7825e45974db0b7c7c94f
Edgar Babajanyan <bedgar2005@gmail.com>
2024-04-14 18:06:25 -0400

    Team members

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Projects from './Projects';
 import videoSrc from './assets/arcDemo.mp4'; // Path to your video file
 import Navbar from "./components/Navbar.jsx";
 import HomePage from "./components/HomePage.jsx";
+import About from "./components/AboutTeam/AboutTeam.jsx"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
                 <Navbar/>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<HomePage />} />
+                    <Route path="/about" element={<About />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/team" element={<HomePage/>} />
                     <Route path="/wiki" element={<HomePage/>} />
