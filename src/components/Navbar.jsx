b9957af7bbd31cd9796f028d9c4ba3730b951b8f
Edgar Babajanyan <bedgar2005@gmail.com>
2024-04-21 18:52:16 -0400

    Video and Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import arcLogo from '../assets/arcicon.png';
 
@@ -27,42 +27,42 @@ const Navbar = () => {
 
     return (
         <>
-        <nav className={`bg-white fixed w-full transition-shadow duration-300 ease-in-out ${isScrolled ? `${isMobileMenuOpen ? '' : 'shadow-lg'} bg-opacity-90` : 'bg-opacity-100'} z-50`}>
-            <div className="px-6 py-3 flex items-center">
-                <Link to="/" className="logo flex items-center space-x-5">
-                    <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'}`}/>
-                    <div className="logo-text text-3xl font-bold text-gray-800 hover:text-gray-700 text-nowrap font-sans">Purdue ARC</div>
-                </Link>
-                <div className="flex-grow"></div>
-                <div className="hidden sm:visible sm:flex items-center space-x-1">
-                    <Link to="/" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Home</Link>
-                    <Link to="/about" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300 text-nowrap">About Us</Link>
-                    <Link to="/projects" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Projects</Link>
-                    {/*  <Link to="/team" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Team</Link> */}
-                    <Link to="/wiki" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Wiki</Link>
+            <nav className={`bg-white fixed w-full transition-shadow duration-300 ease-in-out ${isScrolled ? `${isMobileMenuOpen ? '' : 'shadow-lg'} bg-opacity-90` : 'bg-opacity-100'} z-50`}>
+                <div className="px-6 py-3 flex items-center">
+                    <Link to="/" className="logo flex items-center space-x-5">
+                        <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'}`}/>
+                        <div className="logo-text text-3xl font-bold text-gray-800 hover:text-gray-700 text-nowrap font-sans">Purdue ARC</div>
+                    </Link>
+                    <div className="flex-grow"></div>
+                    <div className="hidden sm:visible sm:flex items-center space-x-1">
+                        <Link to="/" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Home</Link>
+                        <Link to="/about" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300 text-nowrap">About Us</Link>
+                        <Link to="/projects" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Projects</Link>
+                        {/*  <Link to="/team" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Team</Link> */}
+                        <a href="https://wiki.purduearc.com/" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-blue-600 transition duration-300 ease-in-out px-5 py-3 rounded border border-transparent hover:border-gray-300">Wiki</a>
+                    </div>
+                    {isMobile && (
+                        <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600">
+                            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current transition-transform duration-300" style={{transform: isMobileMenuOpen ? 'rotate(-90deg)' : 'rotate(0deg)'}}>
+                                <path d="M4 5h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2z"></path>
+                            </svg>
+                        </button>
+                    )}
                 </div>
-                {isMobile && (
-                    <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600">
-                        <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current transition-transform duration-300" style={{transform: isMobileMenuOpen ? 'rotate(-90deg)' : 'rotate(0deg)'}}>
-                            <path d="M4 5h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2zm0 6h16a1 1 0 000-2H4a1 1 0 000 2z"></path>
-                        </svg>
-                    </button>
+                {isMobileMenuOpen && (
+                    <div
+                        className={`sm:hidden bg-white shadow-lg absolute w-full ${isScrolled ? 'shadow-lg bg-opacity-90' : 'bg-opacity-100'}`}>
+                        <Link to="/" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Home</Link>
+                        <Link to="/about" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">About Us</Link>
+                        <Link to="/projects" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Projects</Link>
+                        {/*  <Link to="/team" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Team</Link> */}
+                        <a href="https://wiki.purduearc.com/" target="_blank" rel="noopener noreferrer" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Wiki</a>
+                    </div>
                 )}
-            </div>
-            {isMobileMenuOpen && (
-                <div
-                    className={`sm:hidden bg-white shadow-lg absolute w-full ${isScrolled ? 'shadow-lg bg-opacity-90' : 'bg-opacity-100'}`}>
-                    <Link to="/" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Home</Link>
-                    <Link to="/about" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">About Us</Link>
-                    <Link to="/projects" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Projects</Link>
-                    {/*  <Link to="/team" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Team</Link> */}
-                    <Link to="/wiki" className="block text-gray-800 hover:text-blue-600 px-6 py-3 rounded border border-transparent hover:border-gray-300">Wiki</Link>
-                </div>
-            )}
-        </nav>
+            </nav>
             <div className="bg-white">
                 <div className="mx-auto px-6 py-3 flex justify-between items-center">
-                        <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'} opacity-0`}/>
+                    <img src={arcLogo} alt="Brand Logo" className={`my-4 transition-all duration-300 ease-in-out ${isScrolled ? 'h-10 w-10' : 'h-16 w-16'} opacity-0`}/>
                 </div>
             </div>
         </>
